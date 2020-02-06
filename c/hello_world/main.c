9dc4d6a8d62496cab1e9112ced3da7c025e131ac
Minnalis <shiedawow@gmail.com>
2020-02-06 01:12:42 +0100

    cleaned the code

--- c/hello_world/main.c
+++ c/hello_world/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main() {
-	printf("hello world\nim trying my best dont bully me owo\nuwu for you\n");
+	printf("hello world\n");
+	printf("im trying my best dont bully me owo\n");
+	printf("uwu for you\n");
 	return 0;
 }
